d7a8b201487f200ef1fc421bcf0af13804874551
trackstar200 <tim.stoddard2@gmail.com>
2016-03-24 00:11:35 -0700

    changed js files slightly

--- js/scraper.js
+++ js/scraper.js
@@ -1,4 +1,5 @@
-// http://www.imdb.com/search/name?birth_monthday=3-20
+// currently uses http://imdb.com
+// possibly update to http://imdb.com/search/name?birth_monthday=3-20
 
 $(function() {
     
@@ -6,18 +7,14 @@ $(function() {
     $.ajax({
         url: "../python/json/info.json",
         success: function(response) {
-            
-            var obj = response;
-            
+                        
             // update page view
             var i = 1;
-            for (var celeb in obj) {
-                console.log(obj[celeb]);
+            for (var celeb in response) {
                 $(`#celeb${i}`).html(`
                     <img src="../python/images/celeb${i}.jpg">
-                    <div class="bottom-text-1">${obj[celeb].name}</div>
-                    <div class="bottom-text-2">${obj[celeb].age} years old</div>
-                `);
+                    <div class="bottom-text-1">${response[celeb].name}</div>
+                    <div class="bottom-text-2">${response[celeb].age} years old</div>`);
                 i++;
             }
         }
